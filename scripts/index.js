cc5231f31f100ed9b15ce93d506159c324e7e1c4
JackSheepe <teterkinvklol@mail.ru>
2023-01-28 18:34:02 +0700

    hotfixes

---
+++ scripts/index.js
@@ -0,0 +1,43 @@
+let btnEdit = document.querySelector(".profile__edit-btn");
+let popup = document.querySelector(".popup");
+let btnPopupClose = document.querySelector(".popup__close-btn");
+
+// Находим форму в DOM
+let formElement = document.querySelector(".popup__form");
+// Находим поля формы в DOM
+let nameInput = document.querySelector("#name-field");
+let bioInput = document.querySelector("#bio-field");
+
+let profileName = document.querySelector(".profile__name"); // Выберите элементы, куда должны быть вставлены значения полей
+let profileBio = document.querySelector(".profile__bio");
+
+function doOpenPopup() {
+  popup.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  bioInput.value = profileBio.textContent;
+  nameInput.title = profileName.textContent;
+  bioInput.title = profileBio.textContent;
+}
+
+function doClosePopup() {
+  popup.classList.remove("popup_opened");
+}
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit(evt) {
+  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+  // Так мы можем определить свою логику отправки.
+  // О том, как это делать, расскажем позже.
+
+  profileName.textContent = nameInput.value;
+  profileBio.textContent = bioInput.value; // Вставьте новые значения с помощью textContent
+
+  doClosePopup();
+}
+
+btnEdit.addEventListener("click", doOpenPopup);
+btnPopupClose.addEventListener("click", doClosePopup);
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener("submit", handleFormSubmit);
