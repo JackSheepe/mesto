3c53fa0426cbd5321aa6dc0a24d6fde720d9d70c
JackSheepe <teterkinvklol@mail.ru>
2023-04-10 00:58:24 +0700

    bugfix/popup-description

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -4,12 +4,15 @@ export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
     this._popupImg = this._popup.querySelector(".popup__img");
+    this._popupDescription = this._popup.querySelector(
+      ".popup__img-description"
+    );
   }
 
   open({ data }) {
     super.open();
     this._popupImg.src = data.link;
     this._popupImg.alt = data.name;
-    this._popupImg.textContent = data.name;
+    this._popupDescription.textContent = data.name;
   }
 }
